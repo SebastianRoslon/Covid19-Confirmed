197f61ee9901114dd6cec9078e1eacbb5bfea863
Sebastian <sebekk7@gmail.com>
2021-01-11 21:16:21 +0100

    init commit

---
+++ src/main/java/pl/roslon/springbootmapinit/Covid19Parser.java
@@ -0,0 +1,61 @@
+package pl.roslon.springbootmapinit;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class Covid19Parser {
+
+    private static final String URL_CONFIRMED = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+    private static final String URL_DEATHS = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv";
+
+    public List<Point> getCovidData() throws IOException {
+        List<Point> points = new ArrayList<>();
+        RestTemplate restTemplate = new RestTemplate();
+
+        String confirmed = restTemplate.getForObject(URL_CONFIRMED, String.class);
+        String deaths = restTemplate.getForObject(URL_DEATHS, String.class);
+
+        StringReader stringReader = new StringReader(confirmed);
+        StringReader stringReaderDeaths = new StringReader(deaths);
+
+        try {
+            CSVParser parse = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(stringReader);
+            CSVParser parseDeaths = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(stringReaderDeaths);
+
+            for (CSVRecord strings : parse) {
+
+                double lat = 0;
+                    if (strings.get("Lat").isEmpty()){
+                        lat = 0;
+                    }else {
+                       lat = Double.parseDouble(strings.get("Lat"));
+                    }
+
+                double lon = 0;
+                if (strings.get("Long").isEmpty()){
+                    lon = 0;
+                }else {
+                    lon = Double.parseDouble(strings.get("Long"));
+                }
+
+                String text = "Zakazeni: " + strings.get("1/10/21");
+                points.add(new Point(lat, lon, text));
+            }
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        return points;
+
+    }
+
+}
---
+++ src/test/java/pl/roslon/springbootmapinit/SpringbootMapInitApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.roslon.springbootmapinit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMapInitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
